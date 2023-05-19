9f7ddf7a9bd3d590566ecbddab3ee4dcd070b64d
Muhammed Musab Demir <mmusabdemirr@gmail.com>
2023-05-19 22:43:49 +0300

    Created EndocrinologyAndMetabolicDiseases AND AnesthesiaAndReanimation

--- src/main/java/hospital/Clinics/EndocrinologyAndMetabolicDiseases.java
+++ src/main/java/hospital/Clinics/EndocrinologyAndMetabolicDiseases.java
@@ -1,4 +1,17 @@
 package hospital.Clinics;
 
-public class EndocrinologyAndMetabolicDiseases {
+import hospital.ClinicsInterface;
+
+public class EndocrinologyAndMetabolicDiseases implements ClinicsInterface<String,String> {
+
+
+    @Override
+    public String clinicName() {
+        return "EndocrinologyAndMetabolicDiseases";
+    }
+
+    @Override
+    public String clinicDoctor() {
+        return "Endocrinology_and_Metabolic_Diseases_Specialist";
+    }
 }
