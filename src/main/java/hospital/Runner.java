128e6556283d50ae55dad3bc3b648800cae1ce70
szgnms <szgnmhmmd@gmail.com>
2023-05-20 14:47:00 +0300

    create wrong selectoin method

--- src/main/java/hospital/Runner.java
+++ src/main/java/hospital/Runner.java
@@ -1,5 +1,7 @@
 package hospital;
 
+
+import hospital.Methods.szgn;
 import hospital.dbConnect.SqlQueries;
 
 public class Runner {
@@ -7,12 +9,6 @@ public class Runner {
     public static void main(String[] args) {
         new SqlQueries().createDoctorTable();
         new SqlQueries().createPatientTable();
-
-        new SqlQueries().printPatientValues("t_patients");
-        new SqlQueries().printDoctorValues("t_doctors");
-        new SqlQueries().addDoctor(1,"ali","veli","test");
-        new SqlQueries().addPatient(1,"ali","veli","test");
-        new SqlQueries().printDoctorWithValue("t_doctors","name","ali");
-        new SqlQueries().printPatientWithValue("t_patients","name","ali");
+      new szgn().hospitalRun();
     }
 }
