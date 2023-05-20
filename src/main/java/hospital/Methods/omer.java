f6a62b4e2edfeb8dd43c4457e69bfc4095fb1ce6
Omer Acar <omeracar34@icloud.com>
2023-05-20 15:17:44 +0300

    doctor menu

--- src/main/java/hospital/Methods/omer.java
+++ src/main/java/hospital/Methods/omer.java
@@ -1,8 +1,43 @@
 package hospital.Methods;
 
+import java.util.Scanner;
+
+import static java.awt.Color.*;
+
 public class omer {
 
+
+    private static String doctorName;
+    private static String doctorSurname;
+    private static String doctorBranch;
+
     void doctorMenu() {
+        System.out.println(GREEN+"================"+WHITE+"DOCTOR MENU"+GREEN+"================");
+        System.out.println(BLUE+"1-"+WHITE+"DOCTOR REGISTRATION");
+        System.out.println(BLUE+"2-"+WHITE+"DELETE DOCTOR");
+        System.out.println(BLUE+"3-"+WHITE+"EXIT");
+
+        szgn.menuSecim=szgn.scan.next();
+
+        switch (szgn.menuSecim){
+            case "1" ->createDoctor();
+            case "2" ->deleteDoctor();
+            case "3" ->System.exit(0);
+            default ->new szgn().wrongInputMethod();
+        }
+
+
     }
+
+    private void createDoctor() {
+    }
+
+
+
+    private void deleteDoctor() {
+    }
+
+
+
 
 }
