3d913be27d73d5af8a68427efa14de41a36121f5
Fazıl Demir <fazildemir20@gmail.com>
2023-05-22 18:06:09 +0300

    branchlist

--- src/main/java/hospital/Runner.java
+++ src/main/java/hospital/Runner.java
@@ -1,15 +1,18 @@
 package hospital;
 
 
+import hospital.Methods.omer;
 import hospital.Methods.szgn;
 import hospital.dbConnect.SqlQueries;
 
 public class Runner {
 
     public static void main(String[] args) {
+
        new SqlQueries().createDischargePatient();
         new SqlQueries().createDoctorTable();
         new SqlQueries().createPatientTable();
       new szgn().hospitalRun();
+
     }
 }
