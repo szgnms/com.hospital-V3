a627a73cb55b67ab97bfdcc31915e3af422f82c9
Fazıl Demir <fazildemir20@gmail.com>
2023-05-24 00:23:50 +0300

    project updated

--- src/main/java/hospital/repo/Rooms.java
+++ src/main/java/hospital/repo/Rooms.java
@@ -5,16 +5,32 @@ import java.util.List;
 
 public class Rooms {
 
-    public static int firstFloorNumberRoom=100;
-    static int secondFloorNumberRoom=200;
-    static int thirdFloorNumberRoom=300;
-    static int fourthFloorNumberRoom=400;
-    static int fifthFloorNumberRoom=500;
-    static int sixthFloorNumberRoom=600;
-    static int seventhFloorNumberRoom=700;
-    static int eighthFloorNumberRoom=800;
-    static int ninthFloorNumberRoom=900;
-    static int tenthFloorNumberRoom=1000;
+    public static int firstFloorNumberRoom = 100;
+    public static int secondFloorNumberRoom = 200;
+    public static int thirdFloorNumberRoom = 300;
+    public static int fourthFloorNumberRoom = 400;
+    public static int fifthFloorNumberRoom = 500;
+    public static int sixthFloorNumberRoom = 600;
+    public static int seventhFloorNumberRoom = 700;
+    public static int eighthFloorNumberRoom = 800;
+    public static int ninthFloorNumberRoom = 900;
+    public static int tenthFloorNumberRoom = 1000;
+
+    public static List<List> floorList = new ArrayList<>();
+
+    public void addFloor() {
+        floorList.add(firstFloorRooms);
+        floorList.add(secondFloorRoooms);
+        floorList.add(thirdFloorRooms);
+        floorList.add(fourthFloorRooms);
+        floorList.add(fifthFloorRooms);
+        floorList.add(sixthFloorRooms);
+        floorList.add(seventhFloorRooms);
+        floorList.add(eighthFloorRooms);
+        floorList.add(eighthFloorRooms);
+        floorList.add(ninthFloorRooms);
+        floorList.add(tenthFloorRooms);
+    }
 
     public static List<Patient> firstFloorRooms = new ArrayList<>();
     static List<Patient> secondFloorRoooms = new ArrayList<>();
@@ -29,7 +45,16 @@ public class Rooms {
 
     static List<Patient> fullRooms = new ArrayList<>();
 
-
+    public static Integer[] firstFloorRoomsArray =new Integer[10];
+    public static Integer[] secondFloorRoomsArray =new Integer[10];
+    public static Integer[] thirdFloorRoomsArray =new Integer[10];
+    public static Integer[] fourthFloorRoomsArray =new Integer[10];
+    public static Integer[] fifthFloorRoomsArray =new Integer[10];
+    public static Integer[] sixthFloorRoomsArray =new Integer[10];
+    public static Integer[] seventhFloorRoomsArray =new Integer[10];
+    public static Integer[] eighthFloorRoomsArray =new Integer[10];
+    public static Integer[] ninthFloorRoomsArray =new Integer[10];
+    public static Integer[] tenthFloorRoomsArray =new Integer[10];
 
 
 }
